6aba89428fb843890946febb81222137d300b5de
BonLeung <654592397@qq.com>
2017-12-14 00:51:59 +0800

    commit

---
+++ src/js/register/mobile/render.js
@@ -0,0 +1,20 @@
+const tpl = (opts = {}) => {
+  return `
+    <div id="register-mobile-wrapper" class="register-mobile-wrapper">
+      <form id="register-mobile-form" onsubmit="return false">
+        <label>
+          <span>手机号</span>
+          <input type="text" name="mobile" placeholder="${opts.mobilePlaceholder || ''}" valid="present mobile" />
+        </label>
+        <label>
+          <span>验证</span>
+          <div id="register-verify-wrapper"></div>
+        </label>
+      </form>
+    </div>
+  `
+}
+
+export default (conf) => {
+  conf.container.innerHTML = tpl(conf)
+}
