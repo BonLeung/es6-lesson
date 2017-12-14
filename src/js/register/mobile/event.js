1cf3f4fb17936197ab0651a7b95122dd610053fa
BonLeung <654592397@qq.com>
2017-12-15 01:50:15 +0800

    commit

--- src/js/register/mobile/event.js
+++ src/js/register/mobile/event.js
@@ -1,9 +1,17 @@
 import Slider from '../../common/slider'
 import { $, addClass, removeClass } from '../../common/utils'
 import { fetchPost } from '../../common/fetch';
+import FormCheck from '../../common/form-check'
 
 export default () => {
   let mobileVerifyToken
+  const $mobileInput = $('register-mobile-input')
+  const $verifyInput = $('register-verify-input')
+  const $verifyBtn = $('register-verify-btn')
+  const $mobileBtn = $('register-mobile-btn')
+  const $verifyMobile = $('register-verify-mobile')
+  const $dialog = $('register-verify-dialog')
+  const $dialogClose = $('register-verify-dialog-close')
 
   const slider = new Slider({
     container: $('register-verify-wrapper'),
--- src/js/register/mobile/render.js
+++ src/js/register/mobile/render.js
@@ -10,7 +10,36 @@ const tpl = (opts = {}) => {
           <span>验证</span>
           <div id="register-verify-wrapper" class="register-verify-wrapper"></div>
         </label>
+        <input id="register-verify-btn" class="disabled" disabled type="submit" value="下一步" />
       </form>
+
+      <div id="register-verify-dialog" class="register-verofy-dialog">
+        <div class="register-verify-dialog-header">
+          <div class="register-verify-dialog-close" id="register-verify-dialog-close"></div>
+        </div>
+        <p class="register-tip">
+          <img src="../images/tip-fill.png" />
+          校验码已发送到你的手机，15分钟内输入有效，请勿泄漏
+        </p>
+        <form id="register-verify-form" onsubmit="return false">
+          <label>
+            <span>手机号</span>
+            <div id="register-verify-mobile"></div>
+          </label>
+          <label>
+            <span>验证码</span>
+            <input id="register-verify-input" type="text" name="verify" />
+          </label>
+          <label>
+            <span>&nbsp;</span>
+            <div class="register-verify-tip">
+              <img src="../images/ok-fill.png" />
+              校验码已发送至你的手机，请查收
+            </div>
+          </label>
+          <input id="register-mobile-btn" class="disabled" disabled type="submit" value="确认" />
+        </form>
+      </div>
     </div>
   `
 }
